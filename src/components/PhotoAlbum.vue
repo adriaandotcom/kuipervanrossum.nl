f9723b0a502b201105c47b0e89e2320c393be609
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2023-08-16 22:43:28 +0200

    Improve controls photo album

--- src/components/PhotoAlbum.vue
+++ src/components/PhotoAlbum.vue
@@ -31,19 +31,63 @@
     </div>
 
     <div class="enlarged" v-if="enlarged" v-on-click-outside="closeImage">
-      <a class="close-icon" @click="closeImage">×</a>
+      <a class="close" @click="closeImage">
+        <svg
+          clip-rule="evenodd"
+          fill-rule="evenodd"
+          stroke-linejoin="round"
+          stroke-miterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 8.933-2.721-2.722c-.146-.146-.339-.219-.531-.219-.404 0-.75.324-.75.749 0 .193.073.384.219.531l2.722 2.722-2.728 2.728c-.147.147-.22.34-.22.531 0 .427.35.75.751.75.192 0 .384-.073.53-.219l2.728-2.728 2.729 2.728c.146.146.338.219.53.219.401 0 .75-.323.75-.75 0-.191-.073-.384-.22-.531l-2.727-2.728 2.717-2.717c.146-.147.219-.338.219-.531 0-.425-.346-.75-.75-.75-.192 0-.385.073-.531.22z"
+            fill-rule="nonzero"
+          />
+        </svg>
+      </a>
       <img :src="selectedImage.src" :alt="selectedImage.title" />
+      <a class="arrow left" @click="prevImage">
+        <svg
+          clip-rule="evenodd"
+          fill-rule="evenodd"
+          stroke-linejoin="round"
+          stroke-miterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m12.012 2c5.518 0 9.997 4.48 9.997 9.998 0 5.517-4.479 9.997-9.997 9.997s-9.998-4.48-9.998-9.997c0-5.518 4.48-9.998 9.998-9.998zm-1.523 6.21s-1.502 1.505-3.255 3.259c-.147.147-.22.339-.22.531s.073.383.22.53c1.753 1.754 3.254 3.258 3.254 3.258.145.145.335.217.526.217.192-.001.384-.074.531-.221.292-.293.294-.766.003-1.057l-1.977-1.977h6.693c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-6.693l1.978-1.979c.29-.289.287-.762-.006-1.054-.147-.147-.339-.221-.53-.222-.19 0-.38.071-.524.215z"
+            fill-rule="nonzero"
+          />
+        </svg>
+      </a>
+      <a class="arrow right" @click="nextImage">
+        <svg
+          clip-rule="evenodd"
+          fill-rule="evenodd"
+          stroke-linejoin="round"
+          stroke-miterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m12.007 2c-5.518 0-9.998 4.48-9.998 9.998 0 5.517 4.48 9.997 9.998 9.997s9.998-4.48 9.998-9.997c0-5.518-4.48-9.998-9.998-9.998zm1.523 6.21s1.502 1.505 3.255 3.259c.147.147.22.339.22.531s-.073.383-.22.53c-1.753 1.754-3.254 3.258-3.254 3.258-.145.145-.335.217-.526.217-.192-.001-.384-.074-.531-.221-.292-.293-.294-.766-.003-1.057l1.977-1.977h-6.693c-.414 0-.75-.336-.75-.75s.336-.75.75-.75h6.693l-1.978-1.979c-.29-.289-.287-.762.006-1.054.147-.147.339-.221.53-.222.19 0 .38.071.524.215z"
+            fill-rule="nonzero"
+          />
+        </svg>
+      </a>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { vOnClickOutside } from "@vueuse/components";
 
-const images = [
+const images = ref([
   {
-    id: 1,
+    id: 0,
     title: "Tekening origineel",
     src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-05-originele-tekening.png",
     source: {
@@ -52,7 +96,7 @@ const images = [
     },
   },
   {
-    id: 2,
+    id: 1,
     title: "Schets uitbouw",
     src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-13-tekening.png",
     source: {
@@ -61,27 +105,27 @@ const images = [
     },
   },
   {
-    id: 3,
+    id: 2,
     title: "Riolering kruipruimte 1",
     src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-1.png",
   },
   {
-    id: 4,
+    id: 3,
     title: "Riolering kruipruimte 2",
     src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-2.png",
   },
   {
-    id: 5,
+    id: 4,
     title: "Riolering kruipruimte 3",
     src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-3.jpg",
   },
   {
-    id: 6,
+    id: 5,
     title: "1935 riool tekening",
     src: "https://files.adriaanvanrossum.nl/curacaostraat/1935-riool-tekening.jpg",
   },
   {
-    id: 7,
+    id: 6,
     title: "Bouwtechnische keuring",
     src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-09-bouwtechnische-keuring-cover.png",
     source: {
@@ -89,20 +133,66 @@ const images = [
       name: "PDF 66mb",
     },
   },
-];
+]);
 
 const enlarged = ref(false);
-const selectedImage = ref({});
+const selectedImageId = ref(null);
+
+const selectedImage = computed(() => {
+  return images.value.find((image) => image.id === selectedImageId.value);
+});
 
 const openImage = (id) => {
   enlarged.value = true;
-  selectedImage.value = images.find((image) => image.id === id);
+  selectedImageId.value = images.value.find((image) => image.id === id).id;
 };
 
 const closeImage = () => {
   enlarged.value = false;
-  selectedImage.value = {};
+  selectedImageId.value = null;
 };
+
+const prevImage = () => {
+  const index = images.value.findIndex(
+    (image) => image.id === selectedImageId.value,
+  );
+  if (index > 0) {
+    selectedImageId.value = images.value[index - 1].id;
+  } else {
+    selectedImageId.value = images.value[images.value.length - 1].id;
+  }
+};
+
+const nextImage = () => {
+  const index = images.value.findIndex(
+    (image) => image.id === selectedImageId.value,
+  );
+  console.log({
+    date: new Date(),
+    index,
+    nextImage: selectedImageId.value,
+    imagesLength: images.value.length,
+  });
+  if (index < images.value.length - 1) {
+    selectedImageId.value = images.value[index + 1].id;
+  } else {
+    selectedImageId.value = images.value[0].id;
+  }
+};
+
+onMounted(() => {
+  const handleKeyDown = (event) => {
+    if (enlarged.value) {
+      if (event.code === "ArrowLeft") {
+        prevImage();
+      } else if (event.code === "ArrowRight") {
+        nextImage();
+      }
+    }
+  };
+  window.addEventListener("keydown", handleKeyDown);
+  return () => window.removeEventListener("keydown", handleKeyDown);
+});
 </script>
 
 <style>
@@ -147,26 +237,33 @@ const closeImage = () => {
   border-radius: 1rem;
   box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
 }
-.close-icon {
+.enlarged .arrow,
+.enlarged .close {
   position: absolute;
-  top: 5px;
-  right: 5px;
-  border: none;
-  font-size: 1.5rem;
+  top: 50%;
+  width: 50px;
+  height: 50px;
+  background-size: 30px 30px;
+  background-repeat: no-repeat;
   cursor: pointer;
+  transform: translateY(-50%);
   opacity: 0.5;
-  font-size: 40px;
-  background-color: black;
-  color: white;
-  border-radius: 1000px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40px;
-  height: 40px;
-  line-height: 40px;
 }
-.close-icon:hover {
-  opacity: 1;
+@media (hover: hover) {
+  .enlarged .arrow:hover,
+  .enlarged .close:hover {
+    opacity: 1;
+    background-color: transparent;
+  }
+}
+.enlarged .close {
+  top: calc(10px + 1rem);
+  right: 10px;
+}
+.enlarged .arrow.left {
+  left: 10px;
+}
+.enlarged .arrow.right {
+  right: 10px;
 }
 </style>
