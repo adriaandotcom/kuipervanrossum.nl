ddd55af0c279570c3e77d46e7dcb14207e10888b
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2023-08-18 14:19:54 +0200

    Add originele bouwtekeningen

--- src/components/PhotoAlbum.vue
+++ src/components/PhotoAlbum.vue
@@ -4,7 +4,7 @@
       <div class="thumbnail" v-for="image in images" :key="image.id">
         <div
           class="image"
-          :style="`background-image:url('${image.src}')`"
+          :style="`background-image:url('${image.thumbnail || image.src}')`"
           @click="openImage(image.id)"
           :alt="image.title"
         />
@@ -31,7 +31,7 @@
     </div>
 
     <div class="enlarged" v-if="enlarged" v-on-click-outside="closeImage">
-      <a class="close" @click="closeImage">
+      <a class="close" @click="closeImage" v-if="!isLoading">
         <svg
           clip-rule="evenodd"
           fill-rule="evenodd"
@@ -46,7 +46,29 @@
           />
         </svg>
       </a>
-      <img :src="selectedImage.src" :alt="selectedImage.title" />
+      <div class="loader" v-if="isLoading">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M13 0c3.667.305 6.863 2.26 8.851 5.129l-1.746 1.013c-1.634-2.273-4.182-3.84-7.105-4.133v-2.009zm-11 12c0-1.47.324-2.863.891-4.122l-1.737-1.007c-.733 1.558-1.154 3.292-1.154 5.129 0 1.837.421 3.571 1.153 5.129l1.738-1.008c-.567-1.259-.891-2.651-.891-4.121zm20 0c0 1.47-.324 2.863-.891 4.122l1.738 1.007c.732-1.558 1.153-3.292 1.153-5.129s-.421-3.571-1.153-5.129l-1.738 1.007c.567 1.259.891 2.652.891 4.122zm-1.895 5.858c-1.634 2.273-4.182 3.84-7.105 4.133v2.009c3.667-.305 6.863-2.26 8.851-5.129l-1.746-1.013zm-16.21-11.717c1.634-2.272 4.183-3.839 7.105-4.132v-2.009c-3.667.305-6.862 2.259-8.851 5.128l1.746 1.013zm7.105 15.85c-2.923-.293-5.471-1.86-7.105-4.133l-1.746 1.013c1.988 2.87 5.184 4.824 8.851 5.129v-2.009z"
+          />
+        </svg>
+      </div>
+      <img
+        :src="selectedImage.src"
+        :alt="selectedImage.title"
+        :style="
+          selectedImage.width && selectedImage.height
+            ? `aspect-ratio: ${selectedImage.width} / ${selectedImage.height}; width: 100%; height: 100%`
+            : ''
+        "
+        @load="onImageLoad"
+        @error="onImageLoad"
+      />
       <a class="arrow left" @click="prevImage">
         <svg
           clip-rule="evenodd"
@@ -62,7 +84,7 @@
           />
         </svg>
       </a>
-      <a class="arrow right" @click="nextImage">
+      <a class="arrow right" @click="nextImage" v-if="!isLoading">
         <svg
           clip-rule="evenodd"
           fill-rule="evenodd"
@@ -85,58 +107,110 @@
 import { computed, onMounted, ref } from "vue";
 import { vOnClickOutside } from "@vueuse/components";
 
-const images = ref([
-  {
-    id: 0,
-    title: "Tekening origineel",
-    src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-05-originele-tekening.png",
-    source: {
-      url: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-05-originele-tekening.fml",
-      name: "FML",
+const images = ref(
+  [
+    {
+      title: "Tekening origineel",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-05-originele-tekening.png",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-05-originele-tekening-thumbnail.jpg",
+      width: 1605,
+      height: 3320,
+      source: {
+        url: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-05-originele-tekening.fml",
+        name: "FML",
+      },
     },
-  },
-  {
-    id: 1,
-    title: "Schets uitbouw",
-    src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-13-tekening.png",
-    source: {
-      url: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-13-tekening.fml",
-      name: "FML",
+    {
+      title: "Schets uitbouw",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-13-tekening.png",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-13-tekening-thumbnail.jpg",
+      width: 1569,
+      height: 3320,
+      source: {
+        url: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-13-tekening.fml",
+        name: "FML",
+      },
     },
-  },
-  {
-    id: 2,
-    title: "Riolering kruipruimte 1",
-    src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-1.png",
-  },
-  {
-    id: 3,
-    title: "Riolering kruipruimte 2",
-    src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-2.png",
-  },
-  {
-    id: 4,
-    title: "Riolering kruipruimte 3",
-    src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-3.jpg",
-  },
-  {
-    id: 5,
-    title: "1935 riool tekening",
-    src: "https://files.adriaanvanrossum.nl/curacaostraat/1935-riool-tekening.jpg",
-  },
-  {
-    id: 6,
-    title: "Bouwtechnische keuring",
-    src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-09-bouwtechnische-keuring-cover.png",
-    source: {
-      url: "https://files.adriaanvanrossum.nl/curacaostraat/bouwtechnische-keuring.pdf",
-      name: "PDF 66mb",
+    {
+      title: "Bouwkundige tekening 2008",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2008-08-18-bouwkundige-tekening.jpg",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/2008-08-18-bouwkundige-tekening-thumbnail.jpg",
+      width: 7100,
+      height: 5190,
     },
-  },
-]);
+    {
+      title: "Kadastrale perceel 2008",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2008-08-18-kadastrale-perceel.jpg",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/2008-08-18-kadastrale-perceel-thumbnail.jpg",
+      width: 7085,
+      height: 5190,
+    },
+    {
+      title: "Riolering kruipruimte 1",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-1.png",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-1-thumbnail.jpg",
+      width: 1852,
+      height: 1058,
+    },
+    {
+      title: "Riolering kruipruimte 2",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-2.png",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-2-thumbnail.jpg",
+      width: 1856,
+      height: 1058,
+    },
+    {
+      title: "Riolering kruipruimte 3",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-3.jpg",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-08-riolering-kruipruimte-3-thumbnail.jpg",
+      width: 911,
+      height: 1280,
+    },
+    {
+      title: "1935 riool tekening",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/1935-riool-tekening.jpg",
+      thumbnail:
+        "https://files.adriaanvanrossum.nl/curacaostraat/1935-riool-tekening-thumbnail.jpg",
+      width: 7768,
+      height: 4668,
+    },
+    {
+      title: "Bouwtechnische keuring",
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2023-08-09-bouwtechnische-keuring.png",
+      width: 200,
+      height: 200,
+      source: {
+        url: "https://files.adriaanvanrossum.nl/curacaostraat/bouwtechnische-keuring.pdf",
+        name: "PDF 66mb!",
+      },
+    },
+    {
+      title: "Lintvoeg- en vloerwaterpassing",
+      width: 200,
+      height: 200,
+      src: "https://files.adriaanvanrossum.nl/curacaostraat/2008-09-01-lintvoeg-en-vloerwaterpassing.jpg",
+      source: {
+        url: "https://files.adriaanvanrossum.nl/curacaostraat/2008-09-01-lintvoeg-en-vloerwaterpassing.pdf",
+        name: "PDF 2mb",
+      },
+    },
+  ].map((image, index) => ({ ...image, id: index })),
+);
 
 const enlarged = ref(false);
 const selectedImageId = ref(null);
+const isLoading = ref(false);
+
+const onImageLoad = () => {
+  isLoading.value = false;
+};
 
 const selectedImage = computed(() => {
   return images.value.find((image) => image.id === selectedImageId.value);
@@ -145,6 +219,7 @@ const selectedImage = computed(() => {
 const openImage = (id) => {
   enlarged.value = true;
   selectedImageId.value = images.value.find((image) => image.id === id).id;
+  isLoading.value = true;
 };
 
 const closeImage = () => {
@@ -161,6 +236,7 @@ const prevImage = () => {
   } else {
     selectedImageId.value = images.value[images.value.length - 1].id;
   }
+  isLoading.value = true;
 };
 
 const nextImage = () => {
@@ -178,6 +254,7 @@ const nextImage = () => {
   } else {
     selectedImageId.value = images.value[0].id;
   }
+  isLoading.value = true;
 };
 
 onMounted(() => {
@@ -187,6 +264,8 @@ onMounted(() => {
         prevImage();
       } else if (event.code === "ArrowRight") {
         nextImage();
+      } else if (event.code === "Escape") {
+        closeImage();
       }
     }
   };
@@ -238,11 +317,14 @@ onMounted(() => {
   transform: translate(-50%, -50%);
 }
 .enlarged img {
+  min-width: 200px;
   max-width: calc(100vw - 2rem);
   max-height: calc(100vh - 2rem);
   border-radius: 1rem;
   box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
+  background-color: #dedee0;
 }
+.enlarged .loader,
 .enlarged .arrow,
 .enlarged .close {
   position: absolute;
@@ -271,5 +353,40 @@ onMounted(() => {
 }
 .enlarged .arrow.right {
   right: 10px;
+}
+.enlarged .loader {
+  width: 100px;
+  height: 100px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.enlarged .loader svg {
+  animation: rotate 3s linear infinite;
+  width: 100px;
+  height: 100px;
+
+  animation:
+    rotate 3s linear infinite,
+    fadeIn 2s;
 }
 </style>
